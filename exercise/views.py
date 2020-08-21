67bc35c41a534a8abaf96096f59b3508a3e7650d
Sohail <sohailshamas@yahoo.com>
2020-08-21 15:12:07 +0000

    Created a view to show exercise plans

--- exercise/views.py
+++ exercise/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import ExercisePlans
 
 # Create your views here.
+
+
+def exerxise_plans(request):
+    """ A view to show exerxise plans """
+
+    exercises = ExercisePlans.objects.all()
+
+    context = {
+        'exercises': exercises
+    }
+    return render(request, 'exercise/exercise_plans.html', context)
