e9cb840b078e2a84310462e1c5f43b4932b4b7af
Sohail <sohailshamas@yahoo.com>
2020-08-21 15:10:00 +0000

    Customized admin

--- exercise/admin.py
+++ exercise/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import ExercisePlans
 
 # Register your models here.
-admin.site.register(ExercisePlans)
+
+
+class ExercisePlanAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'timing',
+        'access',
+        'free_offer',
+        'accompanied_guest',
+        'price',
+
+    )
+
+    ordering = ('price',)
+
+
+admin.site.register(ExercisePlans, ExercisePlanAdmin)
+
