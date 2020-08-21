0450ef80ee4ccac55face479809b522dccc5e402
Sohail <sohailshamas@yahoo.com>
2020-08-21 10:43:08 +0000

    Installed exercise app, created model & added fixtures

---
+++ exercise/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ExercisePlans
+
+# Register your models here.
+admin.site.register(ExercisePlans)
---
+++ exercise/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ExercisePlans(models.Model):
+    name = models.CharField(max_length=80)
+    timing = models.CharField(max_length=120)
+    access = models.CharField(max_length=254)
+    free_offer = models.CharField(max_length=500)
+    accompanied_guest = models.CharField(max_length=220)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
