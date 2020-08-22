86985de91f4703314b0c8e5b93f3996602f43856
Sohail <sohailshamas@yahoo.com>
2020-08-22 15:33:56 +0000

    Created nutrition plan to render nutrition plans

--- nutrition/views.py
+++ nutrition/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import NutritionPlans
 
 # Create your views here.
+
+
+def nutrition_plans(request):
+    """ A view to show nutrition plans """
+
+    nutritions = NutritionPlans.objects.all()
+
+    context = {
+        'nutritions': nutritions
+    }
+    return render(request, 'nutrition/nutrition_plans.html', context)
