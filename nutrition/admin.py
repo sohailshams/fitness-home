3c6e395779de559190c143563878460ce6b3f10e
Sohail <sohailshamas@yahoo.com>
2020-08-22 11:14:59 +0000

    Register nutrition model & customized admin

--- nutrition/admin.py
+++ nutrition/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import NutritionPlans
 
 # Register your models here.
+
+
+class NutritionPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'purpose',
+        'meal_plan',
+        'grocery_list',
+        'message',
+        'coaching',
+        'price',
+
+    )
+
+    ordering = ('price',)
+
+
+admin.site.register(NutritionPlans, NutritionPlanAdmin)
