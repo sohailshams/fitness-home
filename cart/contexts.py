5654f90b15b1c61ccc82e7913bbea22ea154083f
Sohail <sohailshamas@yahoo.com>
2020-08-29 08:04:22 +0000

    Updated contexts.py-made shopping cart content available across the site

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,7 +1,22 @@
+from django.shortcuts import get_object_or_404
+from merchandise.models import Product
+
+
 def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'cart_items': cart_items,
