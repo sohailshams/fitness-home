b1640cbb045723c49939fdb14dd9dbc81c65d27b
Sohail <sohailshamas@yahoo.com>
2020-09-02 19:34:16 +0000

    Restructured add_cart to add items from merchandise, exercise & nutrition apps

--- cart/views.py
+++ cart/views.py
@@ -10,15 +10,37 @@ def view_cart(request):
 
 def add_cart(request, item_id):
     """ Add quantity of specified product to the shopping cart """
-    
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('cart', {})
-    
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    product_type = request.POST.get('product_type')
+    cart = request.session.get('cart', {
+        'merchandise': {},
+        'excercise_plans': {},
+        'nutrition_plans': {},
+    })
+
+    item_in_cart = any([
+        item_id in cart['merchandise'].keys(),
+        item_id in cart['excercise_plans'].keys(),
+        item_id in cart['nutrition_plans'].keys(),
+    ])
+
+    if item_in_cart:
+        if product_type == 'merchandise':
+            cart['merchandise'][item_id] += quantity
+        elif product_type == 'excercise_plan':
+            cart['excercise_plans'][item_id] += quantity
+        else:
+            cart['nutrition_plans'][item_id] += quantity
     else:
-        cart[item_id] = quantity
-        
+        if product_type == 'merchandise':
+            cart['merchandise'][item_id] = quantity
+        elif product_type == 'excercise_plan':
+            cart['excercise_plans'][item_id] = quantity
+        else:
+            cart['nutrition_plans'][item_id] = quantity
+
+    print(cart)
     request.session['cart'] = cart
     return redirect(redirect_url)
