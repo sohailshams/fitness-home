406e19b04d5a00b5ca7182b27b5ee0918993541d
Sohail <sohailshamas@yahoo.com>
2020-09-09 20:39:52 +0000

    Added update url in urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_cart, name='add_cart'),
+    path('update/<item_id>/', views.update_cart, name='update_cart'),
+
 ]
