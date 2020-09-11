c9267293d20ec8b1a465d86d7479cc40bdd75ecd
Sohail <sohailshamas@yahoo.com>
2020-09-11 22:02:04 +0000

    Cleaned up the code

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
-from merchandise.models import Product
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
