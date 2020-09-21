01b87171cc2dbbff3dec616d6d1c141ab20a3e44
Sohail <sohailshamas@yahoo.com>
2020-09-21 09:51:53 +0000

    Added checkout urls in project level urls.py

--- fitness_home/urls.py
+++ fitness_home/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('nutritions/', include('nutrition.urls')),
     path('products/', include('merchandise.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
