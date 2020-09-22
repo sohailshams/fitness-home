f7f41cd4f69d9cabb06970317a7c4ab3a507536c
Sohail <sohailshamas@yahoo.com>
2020-09-22 07:45:56 +0000

    Updated placeholders - added country

--- checkout/forms.py
+++ checkout/forms.py
@@ -32,6 +32,7 @@ class OrderForm(forms.ModelForm):
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County, State or Locality',
+            'country': 'Country',
         }
 
         """
