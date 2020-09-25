e5fa4dd55bcd05695ceaeca6d412bb3aff53caca
Sohail <sohailshamas@yahoo.com>
2020-09-25 10:00:52 +0000

    Created card elements & mounted in checkout.html

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,33 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+// Got stripe PublicKey & clientSecret from template
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+// Created variable using stripe public key
+var stripe = Stripe(stripePublicKey);
+// Created instance of stripe elements
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Ubunto", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#e84610',
+        iconColor: '#e84610'
+    }
+};
+// Created card elements
+var card = elements.create('card', {style: style});
+// Mounted card element to checkout.html div
+card.mount('#card-element');
