66fac6bc684655ba9361d0c4405c9af7e4f2740e
Sohail <sohailshamas@yahoo.com>
2020-09-29 21:11:55 +0000

    Updated checkout function - added handling for the post method

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,12 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import ProductLineItem, ExerciseLineItem, NutritionLineItem
+
+
+from merchandise.models import Product
+from exercise.models import ExercisePlans
+from nutrition.models import NutritionPlans
 from cart.contexts import cart_contents
 
 import stripe
@@ -12,39 +18,103 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {
-        'merchandise_dic': {},
-        'excercise_plans_dic': {},
-        'nutrition_plans_dic': {},
-    })
-    if not cart:
-        messages.error(request,
-                       "There's nothing in your shopping cart at the moment")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {
+            'merchandise_dic': {},
+            'excercise_plans_dic': {},
+            'nutrition_plans_dic': {},
+        })
+        """
+        Put the form data in a dictionay
+        """
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        """
+        Created an instance of the form using form data
+        """
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            """
+            Creation of line items
+            """
+            for product_type, dic in cart.items():
+                if product_type == 'merchandise_dic':
+                    for item_id, quantity in dic.items():
+                        product = Product.objects.get(id=item_id)
+                        order_line_item = ProductLineItem(
+                            order=order,
+                            product=product,
+                            quantity=quantity,
+                        )
+                        order_line_item.save()
+                elif product_type == 'excercise_plans_dic':
+                    for item_id, quantity in dic.items():
+                        product = ExercisePlans.objects.get(id=item_id)
+                        order_line_item = ExerciseLineItem(
+                            order=order,
+                            product=product,
+                            quantity=quantity,
+                        )
+                        order_line_item.save()
+                elif product_type == 'nutrition_plans_dic':
+                    for item_id, quantity in dic.items():
+                        product = NutritionPlans.objects.get(id=item_id)
+                        order_line_item = NutritionLineItem(
+                            order=order,
+                            product=product,
+                            quantity=quantity,
+                        )
+                        order_line_item.save()
 
-    """ Got total from cart_contents """
-    current_cart = cart_contents(request)
-    current_total = current_cart['total']
-    stripe_total = round(current_total * 100)
-    """ Set secret key on stripe """
-    stripe.api_key = stripe_secret_key
-    """ Created payment intent """
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-        )
+        else:
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'))
+            return redirect(reverse('checkout'))
 
-    order_form = OrderForm()
+    else:
+        cart = request.session.get('cart', {
+            'merchandise_dic': {},
+            'excercise_plans_dic': {},
+            'nutrition_plans_dic': {},
+        })
+        if not cart:
+            messages.error(request,
+                           "There is nothing in your \
+                                shopping cart at the moment")
+            return redirect(reverse('products'))
+
+        """ Got total from cart_contents """
+        current_cart = cart_contents(request)
+        current_total = current_cart['total']
+        stripe_total = round(current_total * 100)
+        """ Set secret key on stripe """
+        stripe.api_key = stripe_secret_key
+        """ Created payment intent """
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            )
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
+
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
     return render(request, template, context)
-
