f6bc55887f95e61d3dda230656202996ea35e76b
Sohail <sohailshamas@yahoo.com>
2020-10-04 21:08:37 +0000

    Created cache_checkout_data view

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from django.conf import settings
+from django.views.decorators.http import require_POST
 
 from .forms import OrderForm
 from .models import Order, ProductLineItem, ExerciseLineItem, NutritionLineItem
-
 
 from merchandise.models import Product
 from exercise.models import ExercisePlans
@@ -12,6 +12,31 @@ from nutrition.models import NutritionPlans
 from cart.contexts import cart_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Save user's data
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {
+                'merchandise_dic': {},
+                'excercise_plans_dic': {},
+                'nutrition_plans_dic': {},
+                })),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
