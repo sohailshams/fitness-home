99cea9d7c8afc79862b97700a4eb21e322f5e0de
Sohail <sohailshamas@yahoo.com>
2020-10-06 12:51:18 +0000

    Included profile urls in the project level urls

--- fitness_home/urls.py
+++ fitness_home/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     path('products/', include('merchandise.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
-
-
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
