6201b9f89cae516ee899721517b0487c0c825e1e
Sohail <sohailshamas@yahoo.com>
2020-10-09 08:06:35 +0000

    Added order_history_detail view

--- profiles/views.py
+++ profiles/views.py
@@ -35,3 +35,19 @@ def order_history(request):
 
     return render(request, template, context)
 
+
+def order_history_detail(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
