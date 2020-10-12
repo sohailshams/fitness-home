07157c858e8cf9ada9dc9d365deb05a6bcf66e77
Sohail <sohailshamas@yahoo.com>
2020-10-12 19:48:35 +0000

    Store management-edit produt-added basic view to edit the product

--- merchandise/views.py
+++ merchandise/views.py
@@ -98,3 +98,21 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    """
+    Instantiating product form using the product
+    """
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+
+    template = 'merchandise/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
