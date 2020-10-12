e2b170593ddcd56c79e8e4d0c1da6961d852a855
Sohail <sohailshamas@yahoo.com>
2020-10-12 20:25:49 +0000

    Store management-updated return to product detail page in add_product view

--- merchandise/views.py
+++ merchandise/views.py
@@ -80,9 +80,9 @@ def add_product(request):
         """
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. \
                                     Please make sure, the form is valid.')
