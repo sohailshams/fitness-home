fa7d2a3f18ed6f5a65b29763b220f6b0f66f7e9e
Sohail <sohailshamas@yahoo.com>
2020-10-12 19:16:00 +0000

    Store management - Added post handler for the add new product

--- merchandise/views.py
+++ merchandise/views.py
@@ -73,7 +73,25 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ A view to add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        """
+        Instantiate a new instance of the product form & captured
+        image of the product if submitted.
+        """
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product added successfully!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. \
+                                    Please make sure, the form is valid.')
+    else:
+        """
+        Empty form instantiation
+        """
+        form = ProductForm()
+
     template = 'merchandise/add_product.html'
     context = {
         'form': form,
