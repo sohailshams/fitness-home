666b15e07f70dad69779e7f7b888ed9d735615bc
Sohail <sohailshamas@yahoo.com>
2020-10-13 08:30:36 +0000

    Updated error message for add, edit & delete product

--- merchandise/views.py
+++ merchandise/views.py
@@ -77,7 +77,7 @@ def product_detail(request, product_id):
 def add_product(request):
     """ A view to add a product to the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only store owners can access it.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
@@ -111,7 +111,7 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Aview to edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only store owners can access it.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -148,7 +148,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     """ Aview to delete a product from the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only store owners can access it.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
