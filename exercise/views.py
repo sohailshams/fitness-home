fd98df62856ad872bdf2d505030345de4d6fdd35
Sohail <sohailshamas@yahoo.com>
2020-10-14 11:55:28 +0000

    Store management-added add_exercise view, copied fom merchandise & adjusted

--- exercise/views.py
+++ exercise/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import ExercisePlans
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import ExerciseForm
 
 # Create your views here.
 
@@ -10,6 +13,41 @@ def exercise_plans(request):
     exercises = ExercisePlans.objects.all()
 
     context = {
-        'exercises': exercises
+        'exercises': exercises,
     }
     return render(request, 'exercise/exercise_plans.html', context)
+
+
+@login_required
+def add_exercise(request):
+    """ A view to add a exercise plan to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can access it.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        """
+        Instantiate a new instance of the exercise form & captured
+        image of the exercise plan if submitted.
+        """
+        form = ExerciseForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Exercise Plan added successfully!')
+            return redirect(reverse('exercises',))
+        else:
+            messages.error(request, 'Failed to add exercise plan. \
+                                    Please make sure, the form is valid.')
+    else:
+        """
+        Empty form instantiation
+        """
+        form = ExerciseForm()
+
+    template = 'exercise/add_exercise.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
