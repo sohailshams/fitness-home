2a891f8a6b890f4580962bdadd163d977147143f
Sohail <sohailshamas@yahoo.com>
2020-10-14 14:11:01 +0000

    Store management-added edit_exercise view-copied code from merchandise & adjusted

--- exercise/views.py
+++ exercise/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import ExercisePlans
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -34,7 +34,7 @@ def add_exercise(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Exercise Plan added successfully!')
-            return redirect(reverse('exercises',))
+            return redirect(reverse('exercises'))
         else:
             messages.error(request, 'Failed to add exercise plan. \
                                     Please make sure, the form is valid.')
@@ -51,3 +51,41 @@ def add_exercise(request):
 
     return render(request, template, context)
 
+
+@login_required
+def edit_exercise(request, exercise_id):
+    """ Aview to edit an exercise plan in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can access it.')
+        return redirect(reverse('home'))
+
+    exercise = get_object_or_404(ExercisePlans, pk=exercise_id)
+    if request.method == 'POST':
+        """
+        Instantiated a form using request.post and request.files
+        and told it the specific instance to be updated
+        """
+        form = ExerciseForm(request.POST, request.FILES, instance=exercise)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Exercise Plan {exercise.name} \
+                                        updated successfully!')
+            return redirect(reverse('exercises'))
+        else:
+            messages.error(request, 'Failed to update exercise plan. \
+                            Please ensure the form is valid.')
+    else:
+        """
+        Instantiating exercise plan form using the exercise plan
+        """
+        form = ExerciseForm(instance=exercise)
+        messages.info(request, f'You are editing exercise \
+                                plan {exercise.name}')
+
+    template = 'exercise/edit_exercise.html'
+    context = {
+        'form': form,
+        'exercise': exercise,
+    }
+
+    return render(request, template, context)
