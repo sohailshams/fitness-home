97de8728be1b500d8f9c697ab488a2169f288ffd
Sohail <sohailshamas@yahoo.com>
2020-10-14 14:12:49 +0000

    Store management-created url for edit_exercise view

--- exercise/urls.py
+++ exercise/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.exercise_plans, name='exercises'),
     path('add/', views.add_exercise, name='add_exercise'),
+    path('edit/<int:exercise_id>/', views.edit_exercise, name='edit_exercise'),
 ]
