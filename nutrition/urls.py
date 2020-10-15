bf24feb5cf421e4716efa18f94d5aac39aed4425
Sohail <sohailshamas@yahoo.com>
2020-10-15 08:52:56 +0000

    Store management-created url for delete_nutrition view

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.nutrition_plans, name='nutritions'),
     path('add/', views.add_nutrition, name='add_nutrition'),
     path('edit/<int:nutrition_id>/', views.edit_nutrition, name='edit_nutrition'),
+    path('delete/<int:nutrition_id>/', views.delete_nutrition,
+         name='delete_nutrition')
 ]
