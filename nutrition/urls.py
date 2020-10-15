45212c022bcd42c0f1ea6245c59b25ab5d061e13
Sohail <sohailshamas@yahoo.com>
2020-10-15 08:10:52 +0000

    Store management-created url for edit_nutrition view

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.nutrition_plans, name='nutritions'),
     path('add/', views.add_nutrition, name='add_nutrition'),
+    path('edit/<int:nutrition_id>/', views.edit_nutrition, name='edit_nutrition'),
 ]
