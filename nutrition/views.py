54b54f1bdae42b28e5d9aecd88ac172cdeae7c2f
Sohail <sohailshamas@yahoo.com>
2020-10-15 08:09:35 +0000

    Store management-added edit_nutrition view-copied from edit_exercise & adjusted

--- nutrition/views.py
+++ nutrition/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import NutritionPlans
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -51,3 +51,41 @@ def add_nutrition(request):
 
     return render(request, template, context)
 
+
+@login_required
+def edit_nutrition(request, nutrition_id):
+    """ Aview to edit a nutrition plan in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can access it.')
+        return redirect(reverse('home'))
+
+    nutrition = get_object_or_404(NutritionPlans, pk=nutrition_id)
+    if request.method == 'POST':
+        """
+        Instantiated a form using request.post and request.files
+        and told it the specific instance to be updated
+        """
+        form = NutritionForm(request.POST, request.FILES, instance=nutrition)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Nutrition Plan {nutrition.name} \
+                                        updated successfully!')
+            return redirect(reverse('nutritions'))
+        else:
+            messages.error(request, 'Failed to update nutrition plan. \
+                            Please ensure the form is valid.')
+    else:
+        """
+        Instantiating nutrition plan form using the nutrition plan
+        """
+        form = NutritionForm(instance=nutrition)
+        messages.info(request, f'You are editing nutrition \
+                                plan {nutrition.name}')
+
+    template = 'nutrition/edit_nutrition.html'
+    context = {
+        'form': form,
+        'nutrition': nutrition,
+    }
+
+    return render(request, template, context)
