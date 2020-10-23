50f5208af504372323e74dc5fa8779f36ebada5f
Sohail <sohailshamas@yahoo.com>
2020-10-23 21:48:28 +0000

    Created view to show all reviews

--- review/views.py
+++ review/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Review
 
-# Create your views here.
+
+def all_reviews(request):
+    """ A view to show all reviews """
+
+    reviews = Review.objects.all()
+
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'review/all_review.html', context)
