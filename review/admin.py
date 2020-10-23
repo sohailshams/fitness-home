93d51837faaac52a66b932bacb720016538f732a
Sohail <sohailshamas@yahoo.com>
2020-10-23 21:58:35 +0000

    Registered review model in the admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from review.models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    To display the below fields to the Review admin panel.
+    """
+    list_display = (
+        'user',
+
+    )
+
+
+admin.site.register(Review, ReviewAdmin)
