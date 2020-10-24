8c7f6b682849717b6dd57f1c0489f81fb58612bb
Sohail <sohailshamas@yahoo.com>
2020-10-24 11:27:58 +0000

    Added add_review view-copied code form add_product & adjusted

--- review/views.py
+++ review/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Review
+from django.contrib import messages
+from .forms import ReviewForm
 
 
 def all_reviews(request):
@@ -11,3 +13,34 @@ def all_reviews(request):
         'reviews': reviews
     }
     return render(request, 'review/all_review.html', context)
+
+
+def add_review(request):
+
+    if request.method == 'POST':
+        """
+        Instantiate a new instance of the review form
+        """
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            messages.success(request, 'Your review is added successfully!')
+            return redirect(reverse('reviews'))
+        else:
+            messages.error(request, 'Failed to add your review. \
+                                    Please make sure, the form is valid.')
+    else:
+        """
+        Empty form instantiation
+        """
+        form = ReviewForm()
+
+    template = 'review/add_review.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
+
