945024b6a83a0c7beb252b02dd6c0e90d7cc9446
Sohail <sohailshamas@yahoo.com>
2020-10-24 11:20:23 +0000

    Copied code form forms.py and adjusted

---
+++ review/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        """
+        Telling django it is associated with review model
+        """
+        model = Review
+        """
+        Set exclude attribute and render all fields except user
+        as it will never change
+        """
+        exclude = ('user',)
+        """
+        Override the default init method which allows the form
+        to be customized
+        """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """
+        Remove auto-generated labels
+        """
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0 review-input-form'
+            field.label = False
+
+
