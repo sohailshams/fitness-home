eedaa8a3e1e470dffa5bb49d3758399269eb9edb
Sohail <sohailshamas@yahoo.com>
2020-10-25 18:16:04 +0000

    Created url for edit_review

--- review/urls.py
+++ review/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_reviews, name='reviews'),
     path('add/', views.add_review, name='add_review'),
+    path('edit/<int:review_id>/', views.edit_review, name='edit_review'),
+
 
 ]
