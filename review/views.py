0365ddd8397cd7f486b701305ad4a2211843a735
Sohail <sohailshamas@yahoo.com>
2020-10-25 18:14:17 +0000

    Added edit_review view-copied code from merchandise/edit_product & adjusted

--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Review
 from django.contrib import messages
 from .forms import ReviewForm
@@ -44,3 +44,44 @@ def add_review(request):
     return render(request, template, context)
 
 
+def edit_review(request, review_id):
+    """ A view to edit a review in the store
+    """
+    review = get_object_or_404(Review, pk=review_id)
+    if request.user == review.user:
+
+        review = get_object_or_404(Review, pk=review_id)
+        """
+        If statement to make sure onle review ownwer can access it
+        """
+        if request.method == 'POST':
+            """
+            Instantiated a form using request.post and request.files
+            and told it the specific instance to be updated
+            """
+            form = ReviewForm(request.POST, instance=review)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Your review is \
+                                  updated successfully!')
+                return redirect(reverse('reviews'))
+            else:
+                messages.error(request, 'Failed to update your review. \
+                                Please ensure the form is valid.')
+        else:
+            """
+            Instantiating review form using the review
+            """
+            form = ReviewForm(instance=review)
+            messages.info(request, 'You are editing your review')
+
+        template = 'review/edit_review.html'
+        context = {
+            'form': form,
+            'review': review,
+        }
+
+        return render(request, template, context)
+    else:
+        messages.error(request, 'Sorry, only review owner can access it.')
+        return redirect(reverse('home'))
