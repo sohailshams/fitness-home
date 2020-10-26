d5528c6b4037e74b41f4c3465445bee56a7c5150
Sohail <sohailshamas@yahoo.com>
2020-10-26 17:38:50 +0000

    Updated review model-added update_date field, made your_review field required & made migrations

--- review/models.py
+++ review/models.py
@@ -7,8 +7,9 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              null=True, blank=True,
                              related_name="reviews")
-    your_review = models.TextField(max_length=1500, blank=True, null=True)
+    your_review = models.TextField(max_length=1500)
     date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.username
