9f8c060224818da3c3e7d608a06380ca3c7d68ea
Sohail <sohailshamas@yahoo.com>
2020-10-27 12:30:16 +0000

    Added contact url in project level urls

--- fitness_home/urls.py
+++ fitness_home/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('reviews/', include('review.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
