7ad7dbc0bd82003ec541dcd3ae28df4dd0f713f2
Sohail <sohailshamas@yahoo.com>
2020-10-27 11:50:22 +0000

    Added contact form

---
+++ contact/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    full_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(required=True, widget=forms.Textarea)
+
+    """
+    Override the default init method which allows the form
+    to be customized
+    """
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        
+        """
+        Remove auto-generated labels & added placeholders
+        """
+        for field in self.fields:
+            self.fields[field].label = False 
+            self.fields[field].widget.attrs['class'] = 'rounded-0'
+            self.fields["full_name"].widget.attrs[
+                                        "placeholder"] = "Name"
+            self.fields["email"].widget.attrs[
+                                        "placeholder"] = "Email Address"
+            self.fields["message"].widget.attrs[
+                                        "placeholder"] = "Your Query"
+            
+       
