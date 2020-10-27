bf3763193cb353009403448bbe35d91188b1bab6
Sohail <sohailshamas@yahoo.com>
2020-10-27 12:28:45 +0000

    Added contact view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
-# Create your views here.
+from .forms import ContactForm
+
+
+def contact(request):
+    """
+    View to handle the contact us email functionality
+    """
+    subject = render_to_string(
+            'contact/contact_email/contact_email_subject.txt'
+        )
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            full_name = request.POST.get('full_name', '')
+            email = request.POST.get('email', '')
+            message = request.POST.get('message', '')
+
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    email,
+                    [settings.DEFAULT_FROM_EMAIL],
+                    )
+                messages.success(request, 'Your query has been sent successfully!')
+            except Exception as e:
+                messages.error(request, f"Your query could not be sent,Error! {e}")
+            return redirect(reverse('contact'))
+        else:
+            messages.error(request,
+                           'Sorry something went wrong, please make sure all fields are correctly filled out')
+    else:
+        form = ContactForm()
+
+    template = 'contact/contact_us.html'
+    context = {
+        'form':form,
+    }
+    return render(request, template, context)
+
