3c1187059e75b044083316458df600d0d7ea2dc9
Sohail <sohailshamas@yahoo.com>
2020-11-04 20:38:07 +0000

    Code validation-removed extra lines at the end & cleared too long line errors

--- profiles/models.py
+++ profiles/models.py
@@ -14,13 +14,19 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_full_name = models.CharField(max_length=50, null=True, blank=True)
     default_email = models.EmailField(max_length=254, null=True, blank=True)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True,
+                                            blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True,
+                                               blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True,
+                                               blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True,
+                                            blank=True)
+    default_county = models.CharField(max_length=80, null=True,
+                                      blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country', null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True,
+                                   blank=True)
 
     """
     String method to return user name
@@ -38,6 +44,3 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # If user exists then just save the profile
     instance.userprofile.save()
-
-
-
