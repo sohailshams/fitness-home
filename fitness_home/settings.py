3e9030b2ce181424d1dc0381df67a088032b9a20
Sohail <sohailshamas@yahoo.com>
2020-11-05 21:42:49 +0000

    Added allowed hosts & database_url

--- fitness_home/settings.py
+++ fitness_home/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!g!w3%ud7w6r%*um%(op++kjqyyrz#h6*v4p#wsd_i-fw9(i94'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mss-fitness-home.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'fitness_home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
