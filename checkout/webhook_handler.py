d9c37f506adea0b10bf8515d89313b23045ca58f
Sohail <sohailshamas@yahoo.com>
2020-11-07 18:05:54 +0000

    Correction-indentation in if order_exists block corrected

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -106,10 +106,11 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-                self._send_confirmation_email(order)
-                return HttpResponse(
-                    content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
-                    status=200)
+            self._send_confirmation_email(order)
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified \
+                    order already in database',
+                status=200)
         else:
             order = None
             try:
